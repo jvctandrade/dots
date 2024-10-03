ca5ee3cbc43d43a112c6eaedc7fe8e13501b57bf
João Victor <jvctandrade@gmail.com>
2024-10-03 19:52:01 -0300

    removed fzf of fish and fixed some bugs

--- .config/.config/fish/config.fish
+++ .config/.config/fish/config.fish
@@ -9,8 +9,4 @@ cat ~/.cache/wal/sequences &
 
 alias ls="eza -l --git --color=always --icons=always --sort=extension --no-user --no-permissions"
 alias tree="eza -T --git --color=always --icons=always --sort=extension --no-user --no-permissions"
-
-if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
-  dbus-run-session sway
-  export _JAVA_AWT_WM_NONREPARENTING=1
-end
+alias rm="rm -r"
